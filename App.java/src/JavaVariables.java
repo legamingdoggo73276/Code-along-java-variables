9f7025974d8b63e0db6fe98df19745c3928f507a
Dylan L. <dylanl@hotmail.ca>
2024-04-12 14:24:00 -0400

    Initial commit

---
+++ App.java/src/JavaVariables.java
@@ -0,0 +1,18 @@
+public class JavaVariables {
+//datatypes
+//class level vars
+
+    public static void main(String[] args) throws Exception {
+        //datatype varName [=value];
+        int games = 3;
+        double allowance = 40.5;
+        long battlePass; //we define battlePass
+    
+        battlePass = 15; //we assign a value to battlePass
+    
+        System.out.println("number of games I own = " + games);
+        System.out.println("My monthly allowance = " + allowance);
+        System.out.println("Cost of a single battle pass = " + battlePass);
+    }
+   
+}
